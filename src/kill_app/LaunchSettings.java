9ee673a700f310caf88f57138080a9ce967bc067
Matthieu Baerts <matttbe@gmail.com>
2014-11-27 01:05:39 +0100

    wakeUp before killing and go back home at the end

--- src/kill_app/LaunchSettings.java
+++ src/kill_app/LaunchSettings.java
@@ -2,15 +2,24 @@ package kill_app;
 
 import utils.Utils;
 
+import android.os.RemoteException;
+
 import com.android.uiautomator.core.UiObjectNotFoundException;
 import com.android.uiautomator.testrunner.UiAutomatorTestCase;
 
 public class LaunchSettings extends UiAutomatorTestCase {
 
 	public void testDemo() throws UiObjectNotFoundException {
+		try {
+			getUiDevice().wakeUp();
+		} catch (RemoteException e1) { // not a big deal
+			e1.printStackTrace();
+		}
+
 		String app = getParams().getString("app");
 		assertTrue("No argument 'app'", app != null && !app.isEmpty());
 
 		Utils.killApp(this, app);
+		getUiDevice().pressHome();
 	}
 }
